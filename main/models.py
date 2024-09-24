ddd0f63be733e18ec08f786a28dbbeb003cede36
olav.dendy <olavchristian517@gmail.com>
2024-09-24 21:11:20 +0700

    push tugas4

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 class ProductEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     nama_produk = models.CharField(max_length=255)
     deskripsi = models.TextField()
